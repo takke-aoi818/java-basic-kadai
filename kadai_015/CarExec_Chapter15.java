0ee2b815f44967e7faccf5b74356b50fdea77ecb
takke-aoi818 <159629934+takke-aoi818@users.noreply.github.com>
2024-03-16 23:36:01 +0900

    Add files via upload

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,19 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		
+		//車クラスのインスタンス
+		Car_Chapter15 gear = new Car_Chapter15(1,10);
+		
+		//gearchangeメソッド
+		gear.gearChange(3);
+		
+		//runメソッド
+		gear.run();
+		
+
+	}
+
+}
